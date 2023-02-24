db9e05e16e6f6b867ddeba3420ec8baa877b05a2
SrinivasaKamathR <kamathsri1997@gmail.com>
2023-02-24 22:42:27 +0530

    added reading and writing from file

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //   process.exit();
+  // console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
   const url = req.url;
-  //   if (url === "/") {
-  //     res.write("<html>");
-  //     res.write("<head><title>My First Page</title></head>");
-  //     res.write(
-  //       "<body><form action='/message' method='POST'><input type='text'><button type='submit'>send</button></form></body>"
-  //     );
-  //     res.write("</html>");
-  //     return res.end();
-  //   }
-  //   res.setHeader("Content-Type", "text/html");
-  //   res.write("<html>");
-  //   res.write("<head><title>My Node page</title></head>");
-  //   res.write("<body>Welcome to my Node js Project</body>");
-  //   res.write("</html>");
-  //   res.end();
-
-  if (url === "/home") {
+  const method = req.method;
+  if (url === "/") {
+    const data = fs.readFileSync("./message.txt", "utf8");
     res.write("<html>");
-    res.write("<head><title>My Home page</title></head>");
-    res.write("<body>Welcome to  Home Page</body>");
-    res.write("</html>");
-    return res.end();
-  } else if (url === "/about") {
-    res.write("<html>");
-    res.write("<head><title>My About page</title></head>");
-    res.write("<body>Welcome to About Page</body>");
-    res.write("</html>");
-    return res.end();
-  } else if (url === "/node") {
-    res.write("<html>");
-    res.write("<head><title>My Node page</title></head>");
-    res.write("<body>Welcome to my Node js Project</body>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      `<body><form action="/message" method="POST"><div>${data}</div><input type="text" name="message" /><button type="submit" >Submit</button></form></body>`
+    );
     res.write("</html>");
     return res.end();
   }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      console.log(message);
+      fs.writeFileSync("message.txt", message);
+    });
+    fs.writeFileSync("hello.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.write("<html>");
+  res.write("<head><title>My Node page</title></head>");
+  res.write("<body>Welcome to my Node js Project</body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(4000);
